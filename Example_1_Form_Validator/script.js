d2dc0832f8b993a5cc5f1aae7fbab3289c99544a
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-07-02 22:50:46 +0300

    first commit

---
+++ Example_1_Form_Validator/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("#form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password2");
+
